c2af603bf36d2a9d639b2333b8add6afa6432dc0
bjornwendsjo <73@wend.se>
2022-01-31 14:25:18 +0100

    Initial Commit

---
+++ Do-14-10 WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Do-14-10 WatchKit Extension
+//
+//  Created by bjorn on 2022-01-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Do-14-10 WatchKit Extension/Do_14_10App.swift
@@ -0,0 +1,19 @@
+//
+//  Do_14_10App.swift
+//  Do-14-10 WatchKit Extension
+//
+//  Created by bjorn on 2022-01-31.
+//
+
+import SwiftUI
+
+@main
+struct Do_14_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
